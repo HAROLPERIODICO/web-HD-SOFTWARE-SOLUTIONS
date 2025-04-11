d6ca9541bfcb6796bcecc37f3330181e33f48b16
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-04-11 12:14:47 -0500

    Update NewMenu.js

--- js/NewMenu.js
+++ js/NewMenu.js
@@ -1,13 +1,1 @@
- document.addEventListener("DOMContentLoaded", () => {
-    const navMenu = document.querySelector(".nav__link--menu");
-    const toggleMenu = document.querySelector(".nav__img");
-    const closeMenu = document.querySelector(".nav__close");
 
-    toggleMenu.addEventListener("click", () => {
-        navMenu.classList.add("nav__link--show");
-    });
-
-    closeMenu.addEventListener("click", () => {
-        navMenu.classList.remove("nav__link--show");
-    });
-});
