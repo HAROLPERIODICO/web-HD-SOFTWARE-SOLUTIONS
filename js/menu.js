faf648a0e9c8a71a99c0d701e8f9c20858ec7d1a
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-04-17 09:53:53 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,39 @@
-(function(){
+<script>
+(function () {
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
 
-    openButton.addEventListener('click', ()=>{
+    // Abrir menú
+    openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
     });
 
-    closeMenu.addEventListener('click', ()=>{
+    // Cerrar menú
+    closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     });
 
-    jQuery("html,body").animate({
-	"scrollTop": jQuery(dev).offset().top - 76
-	})
+    // Scroll suave al hacer clic en los enlaces del menú
+    const navLinks = document.querySelectorAll('.nav__link a[href^="#"]');
 
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
 
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 76, // Ajusta si tienes un header fijo
+                    behavior: 'smooth'
+                });
+            }
+
+            // Cerrar el menú (útil en vista móvil)
+            menu.classList.remove('nav__link--show');
+        });
+    });
 })();
+</script>
