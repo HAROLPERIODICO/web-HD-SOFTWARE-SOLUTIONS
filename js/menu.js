0c3892f92281f6763cd6c04568422788c57a3416
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-04-17 10:22:11 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,3 @@
-<script>
 (function () {
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
@@ -36,4 +35,3 @@
         });
     });
 })();
-</script>
